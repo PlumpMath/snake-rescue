f0690306e9a4a02f9666e7da5332370af7bf5b5e
jupiterio <6393827+jupiterio@users.noreply.github.com>
2017-11-07 16:00:58 -0600

    Danger noodle

--- src/entities/Snake.hx
+++ src/entities/Snake.hx
@@ -17,7 +17,7 @@ class Snake extends Pseudo3DSprite {
     
     override public function new(options : OptionalPseudo3DOptions){
         options.frames = 19;
-        options.size = new Vector(21, 9);
+        options.size = new Vector(22, 7);
         
         var image = Luxe.resources.texture("assets/textures/Snake.png");
         image.filter_min = image.filter_mag = FilterType.nearest;
