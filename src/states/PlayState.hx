831c7624d752a8e6d4ffabd1fa4842981bf9c8c3
jupiterio <6393827+jupiterio@users.noreply.github.com>
2017-11-29 17:34:40 -0600

    Yucatec map generation

--- src/states/PlayState.hx
+++ src/states/PlayState.hx
@@ -23,10 +23,9 @@ typedef MapJSONOptions = {
 class PlayState extends State {
     
     var shooter : entities.Pseudo3D;
+    var mapmap : maps.MapMap;
     
-    override public function new(options : StateOptions){
-        super(options);
-        
+    override public function init(){
         Luxe.renderer.clear_color = new luxe.Color().rgb(0x2D1F2B);
         
         createBackground();
@@ -50,18 +49,7 @@ class PlayState extends State {
     }
     
     function createBackground() {
-        var mapmap = new maps.MapMap({
-            x: 0,
-            y: 0,
-            w: 4,
-            h: 4,
-            map_width  : 9,
-            map_height : 9
-        });
-        
-        mapmap.addRoom("az_room0", 0, 0);
-        mapmap.addRoom("az_room1", 1, 0);
-        mapmap.addRoom("az_room0", 2, 1);
+        createYucatecMap();
         
         // for (entity in MapJSONa.entities) {
             // var spr = Main.creators[entity.type](cast {
@@ -73,6 +61,57 @@ class PlayState extends State {
             // Main.sprites.push(spr);
             // Main.colliders.push(spr.collider);
         // }
+    }
+    
+    function createYucatecMap() {
+        mapmap = new maps.MapMap({
+            x: 0,
+            y: 0,
+            w: 5,
+            h: 5,
+            map_width  : 9,
+            map_height : 9
+        });
+        
+        mapmap.addRoom("yu_room0", 0, 0);
+        mapmap.addRoom("yu_room0", 9, 9);
+        for (y in 0...mapmap.h) {
+            if (y < mapmap.h-1) {
+                var placedvert;
+                do {
+                    if (Math.floor(Math.random() * 1.99) == 0) {
+                        placedvert = mapmap.addRoom("yu_room1", Math.floor(Math.random() * 4.99), y);
+                    } else {
+                        placedvert = mapmap.addRoom("yu_room3", Math.floor(Math.random() * 4.99), y);
+                    }
+                } while(!placedvert);
+            }
+            
+            for (x in 0...mapmap.w) {
+                var map = mapmap.getPiece(x, y);
+                if (map == null) {
+                    var placed;
+                    do {
+                        if (Math.floor(Math.random() * 1.99) == 0) {
+                            placed = mapmap.addRoom("yu_room0", x, y);
+                        } else {
+                            placed = mapmap.addRoom("yu_room2", x, y);
+                        }
+                    } while (!placed);
+                }
+            }
+        }
+    }
+    
+    override public function onkeydown(event:KeyEvent) {
+        if (event.keycode == Key.key_r) {
+            mapmap.destroy();
+            
+            createBackground();
+            
+            Main.player.x = 128 + 32/2; // center the player
+            Main.player.y = 160 + 32/2; // inside the tiles
+        }
     }
 
 }
